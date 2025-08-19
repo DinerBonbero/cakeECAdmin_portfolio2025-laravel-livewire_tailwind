94d7af81f5914eccfe686ea3a0a2c65be2d33999
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-19 13:21:30 +0900

    itemsマイグレーションのカラム設定

--- database/migrations/2025_08_16_152203_create_items_table.php
+++ database/migrations/2025_08_16_152203_create_items_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('image', length: 30);
+            $table->string('name', length: 15);
+            $table->mediumInteger('price');
+            $table->string('description', length: 50);
+            $table->tinyInteger('is_pending')->default(0);
         });
     }
 
