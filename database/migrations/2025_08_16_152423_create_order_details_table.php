7ada7a77841bb5e5c5538c527e4abccda2379406
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-19 14:27:32 +0900

    誤字修正oder→order

--- database/migrations/2025_08_16_152423_create_order_details_table.php
+++ database/migrations/2025_08_16_152423_create_order_details_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('oder_details', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('oder_details');
+        Schema::dropIfExists('order_details');
     }
 };
