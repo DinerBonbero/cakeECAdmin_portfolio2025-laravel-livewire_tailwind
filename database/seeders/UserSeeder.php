a55a38631e29b2cc2531f6038504c74c07510d81
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-20 23:58:28 +0900

    Userシーダーに管理者とお客様のマスタデータ作成

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $users = [
+            ['name' => '管理者マスタ', 'email' => 'admin@example.com', 'password' => Hash::make('cakeAdmin'), 'is_admin' => 1],//管理者のマスタデータ
+            ['name' => 'Aさん', 'email' => 'customerA@example.com', 'password' => Hash::make('customerA')],//以下2名　お客様のマスタデータ
+            ['name' => 'Bさん', 'email' => 'customerB@example.com', 'password' => Hash::make('customerB')]
+        ];
+
+
+        foreach ($users as $user) {//管理者・お客様のマスタデータの登録
+            DB::table('users')->insert($user);
+        }
     }
 }
