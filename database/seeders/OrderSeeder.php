89061fc3d2b234f0bb9eafd3dac3b03616a2eedc
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-21 18:06:55 +0900

    誤字修正とテキスト整形

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -13,25 +13,24 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        $userId = [];
+        $userIds = [];
 
         $emails = [
             'customerA@example.com',
             'customerB@example.com'
         ];
 
-        foreach ($emails as $email) {//Userシーダに登録したメールと一致するお客様A,Bのマスタデータのidを取得
-            $id = DB::table('users')->where('email', $email)->value('id');//getだとコレーション(配列ではない)が返ってくるためvalueでレコードから値を取得
-            $userIds[] = $id;//コレーションだとエラーになる
+        foreach ($emails as $email) { //Userシーダに登録したメールと一致するお客様A,Bのマスタデータのidを取得
+            $id = DB::table('users')->where('email', $email)->value('id'); //getだとコレーション(配列ではない)が返ってくるためvalueでレコードから値を取得
+            $userIds[] = $id; //コレーションだとエラーになる
         }
 
         $orders = [
-            ['user_id' => $userIds[0], 'date' => '2025-06-17', 'is_shipped' => 0],//以下2名　お客様A,Bの注文マスタデータ
+            ['user_id' => $userIds[0], 'date' => '2025-06-17', 'is_shipped' => 0], //以下2名　お客様A,Bの注文マスタデータ
             ['user_id' => $userIds[1], 'date' => '2025-08-21', 'is_shipped' => 1]
         ];
 
-
-        foreach ($orders as $order) {//管理者・お客様のマスタデータの登録
+        foreach ($orders as $order) { //管理者・お客様のマスタデータの登録
             DB::table('orders')->insert($order);
         }
     }
