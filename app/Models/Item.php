49ba38304b3cf7060edb53c5bacc6ce3bac0f160
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-23 11:11:43 +0900

    リレーション:hasMany(Cart::class)、hasMany(OrderDetail::class)の記述

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Item extends Model
 {
-    //
+    public function carts(): HasMany
+    {
+        return $this->hasMany(Cart::class);
+    }
+    
+    public function order_details(): HasMany
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
 }
