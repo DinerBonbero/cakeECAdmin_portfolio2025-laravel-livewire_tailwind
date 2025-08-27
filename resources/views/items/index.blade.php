c49c3c0ea62ce7e1c5580009bd21083014b6a4ae
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-27 15:57:10 +0900

    ログインユーザー用の商品一覧

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -18,3 +18,24 @@
         </div>
     </x-layouts.app.guest-header>
 @endguest
+
+@auth
+    <x-layouts.app.login-header>
+        <div class="grid grid-cols-3 gap-10">
+            @foreach ($items as $item)
+                <div class="bg-white p-1">
+                    <img src="{{ asset('images/' . $item->image) }}">
+                    <div class="flex">
+                        <span class="flex-none mx-2">{{ $item->name }}</span>
+                        <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="flex justify-center my-5">
+            <span>
+                {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
+            </span>
+        </div>
+    </x-layouts.app.guest-header>
+@endauth
