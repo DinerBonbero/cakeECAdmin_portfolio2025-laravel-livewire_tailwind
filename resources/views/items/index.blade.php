d3b80466c88f69569b9de50735b25fb435683235
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-27 14:09:53 +0900

    商品一覧大方完成

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,5 +1,18 @@
-<x-layouts.app.guest-header>
-    @foreach ($items as $item)
-        <div>{{ $item->name }}</div>
-    @endforeach
-</x-layouts.app.guest-header>
+@guest
+    <x-layouts.app.guest-header>
+        <div class="grid grid-cols-3 gap-2">
+            @foreach ($items as $item)
+                <div class="bg-white p-1">
+                    <img src="{{ asset('images/' . $item->image) }}">
+                    <div class="flex">
+                        <span class="flex-none mx-2">{{ $item->name }}</span>
+                        <span class="flex-1">{{ '(税込み)' . number_format($item->price) . '円' }}</span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="flex items-center">
+            {{ $items->onEachSide(1)->links() }}
+        </div>
+    </x-layouts.app.guest-header>
+@endguest
