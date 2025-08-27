f89b942195a4da9030cad539c9650a64235bc903
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-27 15:27:49 +0900

    ペジネーション、金額の3桁区切り、レイアウト調整

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,18 +1,20 @@
 @guest
     <x-layouts.app.guest-header>
-        <div class="grid grid-cols-3 gap-2">
+        <div class="grid grid-cols-3 gap-10">
             @foreach ($items as $item)
                 <div class="bg-white p-1">
                     <img src="{{ asset('images/' . $item->image) }}">
                     <div class="flex">
                         <span class="flex-none mx-2">{{ $item->name }}</span>
-                        <span class="flex-1">{{ '(税込み)' . number_format($item->price) . '円' }}</span>
+                        <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
                     </div>
                 </div>
             @endforeach
         </div>
-        <div class="flex items-center">
-            {{ $items->onEachSide(1)->links() }}
+        <div class="flex justify-center my-5">
+            <span>
+                {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
+            </span>
         </div>
     </x-layouts.app.guest-header>
 @endguest
