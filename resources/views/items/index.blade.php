d55c279054470262d7abcdc2af126ba3c713737d
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-28 10:30:18 +0900

    @auth用のHTML分コメントアウト

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,26 +1,25 @@
-@guest
-    <x-layouts.app.guest-header>
-        <div class="py-10"></div>
-        <div class="grid grid-cols-3 gap-10">
-            @foreach ($items as $item)
-                <div class="bg-white p-1">
-                    <img src="{{ asset('images/' . $item->image) }}">
-                    <div class="flex">
-                        <span class="flex-none mx-2">{{ $item->name }}</span>
-                        <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
-                    </div>
+<x-layouts.app.guest-header>
+    <div class="py-10"></div>
+    <div class="grid grid-cols-3 gap-10">
+        @foreach ($items as $item)
+            <div class="bg-white p-1">
+                <img src="{{ asset('images/' . $item->image) }}">
+                <div class="flex">
+                    <span class="flex-none mx-2">{{ $item->name }}</span>
+                    <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
                 </div>
-            @endforeach
-        </div>
-        <div class="flex justify-center my-5">
-            <span>
-                {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
-            </span>
-        </div>
-    </x-layouts.app.guest-header>
-@endguest
+            </div>
+        @endforeach
+    </div>
+    <div class="flex justify-center my-5">
+        <span>
+            {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
+        </span>
+    </div>
+</x-layouts.app.guest-header>
 
-@auth
+
+{{-- @auth
     <x-layouts.app.login-header>
         <div class="py-10"></div>
         <div class="grid grid-cols-3 gap-10">
@@ -40,4 +39,4 @@
             </span>
         </div>
     </x-layouts.app.guest-header>
-@endauth
+@endauth --}}
