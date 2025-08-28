4d818efebf1c27abe7928b45d5eb80a30d66eff2
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-28 14:39:01 +0900

    コメントアウト削除　HTML分最少に設定

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app.guest-header>
+<x-layouts.app.header>
     <div class="py-10">
         @can('is_admin')
             <span class="text-emerald-500 text-2xl">管理者</span>
@@ -20,27 +20,4 @@
             {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
         </span>
     </div>
-</x-layouts.app.guest-header>
-
-
-{{-- @auth
-    <x-layouts.app.login-header>
-        <div class="py-10"></div>
-        <div class="grid grid-cols-3 gap-10">
-            @foreach ($items as $item)
-                <div class="bg-white p-1">
-                    <img src="{{ asset('images/' . $item->image) }}">
-                    <div class="flex">
-                        <span class="flex-none mx-2">{{ $item->name }}</span>
-                        <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        <div class="flex justify-center my-5">
-            <span>
-                {{ $items->onEachSide(1)->links('vendor.pagination.tailwind') }}
-            </span>
-        </div>
-    </x-layouts.app.guest-header>
-@endauth --}}
+</x-layouts.app.header>
