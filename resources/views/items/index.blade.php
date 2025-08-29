0db3e97777b61c4be13730688202a41d4cf83057
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-29 19:36:22 +0900

    商品カードのaタグと遷移先設定

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -6,13 +6,15 @@
     </div>
     <div class="grid grid-cols-3 gap-10">
         @foreach ($items as $item)
-            <div class="bg-white p-1">
-                <img src="{{ asset('images/' . $item->image) }}">
-                <div class="flex">
-                    <span class="flex-none mx-2">{{ $item->name }}</span>
-                    <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
+            <a href="{{ route('items.show', $item) }}">
+                <div class="bg-white p-1">
+                    <img src="{{ asset('images/' . $item->image) }}">
+                    <div class="flex">
+                        <span class="flex-none mx-2">{{ $item->name }}</span>
+                        <span class="flex-1">{{ '(税込み)' . ' ' . number_format($item->price) . '円' }}</span>
+                    </div>
                 </div>
-            </div>
+            </a>
         @endforeach
     </div>
     <div class="flex justify-center my-5">
