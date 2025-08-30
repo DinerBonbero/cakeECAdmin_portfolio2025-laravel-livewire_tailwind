6f99bf81df02f57ce6d0dc398aac4528698447ce
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-30 11:00:19 +0900

    詳細追加　HTML調整中

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -4,7 +4,12 @@
             <span class="text-emerald-500 text-2xl">管理者</span>
         @endcan
     </div>
-    <div class="">
-        <img src="{{ asset('/images/' . $item->image) }}">
+    <div class="w-2/3 mx-auto">
+        <img class="w-1/2" src="{{ asset('/images/' . $item->image) }}">
+        <div class="flex flex-col">
+        <span>{{ $item->name }}</span>
+        <span>{{ $item->price }}</span>
+        <span><x-button.></span>
+        </div>
     </div>
 </x-layouts.app.header>
