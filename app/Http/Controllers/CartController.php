1d80f7f8a893ca3afa4a3c0e9151dc288d160d7a
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-08-31 17:40:46 +0900

    storeメソッド記述

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\Cart;
 
 class CartController extends Controller
 {
-    public function store(){
-        
+    public function store(Item $item, Request $request)
+    {
+        Cart::create(['user_id' => auth()->id], ['item_id' => $item->id], ['item_num' => $request->item]);
     }
 }
