a70fe9c799ebef9fcfd696dfa701f00827f3bd51
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-03 12:00:53 +0900

    ボタンコンポーネントのタグ化(視認化)

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -38,12 +38,13 @@
         </div>
         <div class="text-center pb-10 w-full">
             <p class="pb-5">{{ $item->description }}</p>
-            @component('components.button.return')
+            {{-- @component('components.button.return')
             戻る
             @slot('message')
-            href=" {{ route('items.index') }} "
+            href="戻る"
             @endslot
-            @endcomponent
+            @endcomponent --}}
+            <x-button.return message="戻る" href="{{ route('items.index') }}" />
         </div>
     </div>
 </x-layouts.app.header>
