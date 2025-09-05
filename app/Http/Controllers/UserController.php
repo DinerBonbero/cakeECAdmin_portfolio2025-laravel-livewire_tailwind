2608bfa65d05c1118ab3934cd2a2b5dc21f9a70e
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-05 21:58:28 +0900

    ユーザー情報の登録処理記述

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\UserInfoRequest;
+use App\Models\UserInfo;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function create(){
+    public function create()
+    {
 
         return view('user_info.create');
     }
 
-    public function store(UserInfoRequest $request){
+    public function store(UserInfoRequest $request)
+    {
 
-        $validated = $request->validated();
-        dd($validated);
-        exit();
+        $validated = $request->validated();//連想配列
+        // var_dump($validated);
+        // $validated['first_name'];
+        // exit();
+
+        UserInfo::create([
+            'user_id' => Auth::id(),
+            'last_name' => $validated['last_name'],
+            'first_name' => $validated['first_name'],
+            'phone_number' => $validated['phone_number'],
+            'postal_code' => $validated['postal_code'],
+            'prefecture' => $validated['prefecture'],
+            'street_address' => $validated['street_address'],
+            'address_detail' => $validated['address_detail'],
+        ]);
+        
         return redirect()->route('items.index');
     }
 }
