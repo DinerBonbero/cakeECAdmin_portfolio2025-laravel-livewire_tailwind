8f4fb3f1cc3c7a59346c067095046be05565c7dc
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-05 21:06:55 +0900

    バリデーションのルール・メッセージ・authorize()のtrue記述

--- app/Http/Requests/UserInfoRequest.php
+++ app/Http/Requests/UserInfoRequest.php
@@ -13,7 +13,7 @@ class UserInfoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -26,11 +26,32 @@ class UserInfoRequest extends FormRequest
         return [
             'last_name' => 'required|max:30',
             'first_name' => 'required|max:30',
-            'phone_number' => 'required|string|regex:^\d{2,4}-\d{2,4}-\d{4}$',
-            'postal_code' => 'required|string|regex:^\d{3}-\d{4}$',
+            'phone_number' => 'required|string|regex:/^\d{2,4}-\d{2,4}-\d{4}$/',
+            'postal_code' => 'required|string|regex:/^\d{3}-\d{4}$/',
             'prefecture' => ['required', Rule::in(PrefectureConst::List)],//Ruleファサードや定数、変数などが作動するように配列に入れて文字列ルールは文字列、ファサードや定数・変数をきっちり分けて要素として扱う
             'street_address' => 'required|max:50',
             'address_detail' => 'max:50'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'last_name.required' => '姓を入力してください。',
+            'last_name.max' => '姓は30文字以内で入力してください。',
+            'first_name.required' => '名を入力してください。',
+            'first_name.max' => '名は30文字以内で入力してください。',
+            'phone_number.required' => '電話番号を入力してください。',
+            'phone_number.string' => '電話番号はテキストで入力してください。',
+            'phone_number.regex' => '電話番号は半角数字ハイフンありの(2～4桁)-(2～4桁)-(4桁)​の形式で入力してください。',
+            'postal_code.required' => '郵便番号を入力してください。',
+            'postal_code.string' => '郵便番号はテキストで入力してください。',
+            'postal_code.regex' => '郵便番号は半角数字ハイフンありの(3桁-4桁)の形式で入力してください。',
+            'prefecture.required' => '都道府県を選択してください。',
+            'prefecture.in' => 'この値は無効です、選択肢から選んでください。',
+            'street_address.required' => '市区町村・番地を入力してください。',
+            'street_address.max' => '市区町村・番地は50文字以内で入力してください。',
+            'address_detail.max' => '建物名・部屋番号は50文字以内で入力してください。'
         ];
     }
 }
