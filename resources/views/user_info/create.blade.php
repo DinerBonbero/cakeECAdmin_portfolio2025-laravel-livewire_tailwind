c90fd8cd1ac97fd19605ca317f77a3f2a1396511
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-06 13:14:49 +0900

    バリデーションのエラーメッセージ記述

--- resources/views/user_info/create.blade.php
+++ resources/views/user_info/create.blade.php
@@ -12,6 +12,11 @@
                             </label>
                             <input type="text" id="last_name" name="last_name" value="{{ old('last_name') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm" placeholder="苺野">
+                            @error('last_name')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                         <div>
                             <label for="first_name" class="block">
@@ -19,9 +24,13 @@
                             </label>
                             <input type="text" id="first_name" name="first_name" value="{{ old('first_name') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm" placeholder="慶喜">
+                            @error('first_name')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                     </div>
-
                     <div class="text-left text-base">
                         <div class="mb-5">
                             <label for="phone_number" class="block">電話番号</label>
@@ -29,12 +38,22 @@
                                 value="{{ old('phone_number') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="090-1234-5678">
+                            @error('phone_number')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                         <div class="mb-5">
                             <label for="postal_code" class="block">郵便番号</label>
                             <input type="text" id="postal_code" name="postal_code" value="{{ old('postal_code') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="012-3456">
+                            @error('postal_code')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                         <div class="mb-5">
                             <label for="prefecture" class="block">都道府県</label>
@@ -42,14 +61,17 @@
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full">
                                 <option value="">選択してください</option>
                                 @foreach (App\Consts\PrefectureConst::List as $prefecture)
-                                    <option value="{{ $prefecture }}" @if (old('prefecture') == $prefecture) selected @endif>
+                                    <option value="{{ $prefecture }}"
+                                        @if (old('prefecture') == $prefecture) selected @endif>
                                         {{ $prefecture }}
                                     </option>
                                 @endforeach
                             </select>
-
-                            {{-- <input type="text" id="prefecture" name="prefecture" value="{{ old('prefecture') }}"
-                                class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"> --}}
+                            @error('prefecture')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                         <div class="mb-5">
                             <label for="street_address" class="block">市区町村・番地</label>
@@ -57,6 +79,11 @@
                                 value="{{ old('street_address') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="大阪市阿倍野区おかし村1-2-2">
+                            @error('street_address')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                         <div class="mb-5">
                             <label for="address_detail" class="block">建物名・部屋番号</label>
@@ -64,9 +91,13 @@
                                 value="{{ old('address_detail') }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="あまあまハイツ 175号室">
+                            @error('address_detail')
+                                <span class="block text-rose-500">
+                                    {{ $message }}
+                                </span>
+                            @enderror
                         </div>
                     </div>
-
                     <div class="mx-auto text-center">
                         <x-button.brown message="登録" />
                     </div>
