dd7eb131c6792cf683e1319fc40f423ae3e052cd
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-06 11:03:54 +0900

    ログアウトをaタグからpost送信に変更・レイアウト調整

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -9,7 +9,8 @@
     <div class="w-auto text-center">
         <img class="w-4/9 py-2 inline" src="{{ asset('images/logo.png') }}" />
         @can('user')
-            <a href="route('mycart_items.index')"><img class="size-20 inline absolute right-0 top-0"src="{{ asset('images/icon_cart.png') }}" /></a>
+            <a href="route('mycart_items.index')"><img
+                    class="size-20 inline absolute right-0 top-0"src="{{ asset('images/icon_cart.png') }}" /></a>
         @endcan
     </div>
 
@@ -27,15 +28,22 @@
                 <summary class="px-7 text-center lg:ms-0 text-lg list-none"><span>{{ Auth::user()->name }}</span></summary>
                 <span class="list-none text-left">
                     @can('user')
-                        <li class="py-1"><a href="{{route('user_info.create')}}" wire:navigate>ユーザー情報の設定</a></li>
-                        <li class="py-1"><a href="{{route('order.history')}}" wire:navigate>購入履歴</a></li>
+                        <li class="py-1"><a href="{{ route('user_info.create') }}" wire:navigate>ユーザー情報の設定</a></li>
+                        <li class="py-1"><a href="{{ route('order.history') }}" wire:navigate>購入履歴</a></li>
                     @endcan
                     @can('is_admin')
-                        <li class="py-1"><a href="{{route('sales.history')}}" wire:navigate>販売履歴</a></li>
-                        <li class="py-1"><a href="{{route('items.create')}}" wire:navigate>商品登録</a></li>
+                        <li class="py-1"><a href="{{ route('sales.history') }}" wire:navigate>販売履歴</a></li>
+                        <li class="py-1"><a href="{{ route('items.create') }}" wire:navigate>商品登録</a></li>
                     @endcan
-                    <li class="py-1"><a href="{{route('user_password.edit')}}" wire:navigate>パスワード変更</a></li>
-                    <li class="py-1"><a href="{{route('logout')}}" wire:navigate>ログアウト</a></li>
+                    <li class="py-1"><a href="{{ route('user_password.edit') }}" wire:navigate>パスワード変更</a></li>
+                    <li class="py-1">
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="w-full text-left">
+                                ログアウト
+                            </button>
+                        </form>
+                    </li>
                 </span>
             </details>
         @else
