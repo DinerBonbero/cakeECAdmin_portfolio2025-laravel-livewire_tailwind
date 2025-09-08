cd0cd037b0c3e438b55fb99414ccec6623d8fc32
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-08 10:54:55 +0900

    入力欄の初期値に登録したDBの値を表示

--- resources/views/user_info/edit.blade.php
+++ resources/views/user_info/edit.blade.php
@@ -11,7 +11,7 @@
                             <label for="last_name" class="block">
                                 姓
                             </label>
-                            <input type="text" id="last_name" name="last_name" value="{{ old('last_name') }}"
+                            <input type="text" id="last_name" name="last_name" value="{{ old('last_name', $userInfo->last_name) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm" placeholder="苺野">
                             @error('last_name')
                                 <span class="block text-rose-500">
@@ -23,7 +23,7 @@
                             <label for="first_name" class="block">
                                 名
                             </label>
-                            <input type="text" id="first_name" name="first_name" value="{{ old('first_name') }}"
+                            <input type="text" id="first_name" name="first_name" value="{{ old('first_name', $userInfo->first_name) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm" placeholder="慶喜">
                             @error('first_name')
                                 <span class="block text-rose-500">
@@ -36,7 +36,7 @@
                         <div class="mb-5">
                             <label for="phone_number" class="block">電話番号</label>
                             <input type="tel" id="phone_number" name="phone_number"
-                                value="{{ old('phone_number') }}"
+                                value="{{ old('phone_number', $userInfo->phone_number) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="090-1234-5678">
                             @error('phone_number')
@@ -47,7 +47,7 @@
                         </div>
                         <div class="mb-5">
                             <label for="postal_code" class="block">郵便番号</label>
-                            <input type="text" id="postal_code" name="postal_code" value="{{ old('postal_code') }}"
+                            <input type="text" id="postal_code" name="postal_code" value="{{ old('postal_code', $userInfo->postal_code) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="012-3456">
                             @error('postal_code')
@@ -63,7 +63,7 @@
                                 <option value="">選択してください</option>
                                 @foreach (App\Consts\PrefectureConst::List as $prefecture)
                                     <option value="{{ $prefecture }}"
-                                        @if (old('prefecture') == $prefecture) selected @endif>
+                                        @if (old('prefecture', $userInfo->prefecture) == $prefecture) selected @endif>
                                         {{ $prefecture }}
                                     </option>
                                 @endforeach
@@ -77,7 +77,7 @@
                         <div class="mb-5">
                             <label for="street_address" class="block">市区町村・番地</label>
                             <input type="text" id="street_address" name="street_address"
-                                value="{{ old('street_address') }}"
+                                value="{{ old('street_address', $userInfo->street_address) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="大阪市阿倍野区おかし村1-2-2">
                             @error('street_address')
@@ -89,7 +89,7 @@
                         <div class="mb-5">
                             <label for="address_detail" class="block">建物名・部屋番号</label>
                             <input type="text" id="address_detail" name="address_detail"
-                                value="{{ old('address_detail') }}"
+                                value="{{ old('address_detail', $userInfo->address_detail) }}"
                                 class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                                 placeholder="あまあまハイツ 175号室">
                             @error('address_detail')
