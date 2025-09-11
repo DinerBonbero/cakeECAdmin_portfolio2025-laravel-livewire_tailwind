c4d4bd35709d7c88e955c07e31f40aae7c493331
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-12 00:23:47 +0900

    wireコンポーネントを分けてエラー発生していたため一つにまとめてfix

--- resources/views/mycart/index.blade.php
+++ resources/views/mycart/index.blade.php
@@ -21,22 +21,7 @@
                     <div class="flex flex-col ml-25 w-full text-left">
                         <span class="mt-2">{{ $cartItem->item->name }}</span>
                         <div class="w-full">
-                            <div class="mt-5 inline-block relative w-4/11">
-                                <livewire:decrement>
-                                {{-- <button wire:click="decrement({{ $cartItem->item_id }})"
-                                    class="text-gray-400 text-2xl absolute left-2">
-                                    -
-                                </button> --}}
-                                <input readonly type="number" id="item_num" name="item_num"
-                                    value="{{ old('item_num', $cartItem->item_num) }}" min="1" max="10"
-                                    step="1"
-                                    class="w-full bg-white border-3 border-solid border-gray-300 rounded-sm text-center hide-spin">
-                                {{-- <button wire:click="increment({{ $cartItem->item_id }})"
-                                    class="text-gray-400 text-2xl absolute right-2">
-                                    +
-                                </button> --}}
-                                <livewire:increment>
-                            </div>
+                            <livewire:CartItemNum :cartItem="$cartItem" />
                             <label for="item_num" class="ml-10">個</label>
                         </div>
                     </div>
