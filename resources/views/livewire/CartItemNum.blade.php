ad80d87b59704447ce5bf3ac749359554766e9d5
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-12 00:28:24 +0900

    ルート要素が複数によりエラー発生していたためdivで一つにまとめるfix ※ルート要素＝最上位のタグ

--- resources/views/livewire/CartItemNum.blade.php
+++ resources/views/livewire/CartItemNum.blade.php
@@ -1,9 +1,13 @@
-<button wire:click="decrement({{ $cartItem->item_id }})" class="text-gray-400 text-2xl absolute left-2">
+<div class="mt-5 inline-block relative w-4/11">
+<button wire:click="update('-')" class="text-gray-400 text-2xl absolute left-2">
     -
 </button>
 <input readonly type="number" id="item_num" name="item_num" value="{{ old('item_num', $cartItem->item_num) }}"
     min="1" max="10" step="1"
     class="w-full bg-white border-3 border-solid border-gray-300 rounded-sm text-center hide-spin">
-<button wire:click="increment({{ $cartItem->item_id }})" class="text-gray-400 text-2xl absolute right-2">
+<button wire:click="update('+')" class="text-gray-400 text-2xl absolute right-2">
     +
 </button>
+{{-- 必ずdivで囲む 囲まないと動的サイトとして認識しない --}}
+</div>
+
