5504162f82b6c6a0ec5ba6be05cf2f082b1287e9
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-11 15:22:53 +0900

    増減コンポーネント作成：未完成

---
+++ resources/views/livewire/decrement.blade.php
@@ -0,0 +1,3 @@
+<button wire:click="update" class="text-gray-400 text-2xl absolute left-2">
+    -
+</button>
---
+++ resources/views/livewire/increment.blade.php
@@ -0,0 +1,3 @@
+<button wire:click="update" class="text-gray-400 text-2xl absolute right-2">
+    +
+</button>
