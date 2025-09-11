1118ea3a00f8c9c88233700b22ea115fd1c14fc5
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-12 00:30:41 +0900

    コンポーネントを一つにまとめたため表示するrenderメソッドのコンポーネントも一つで表示
    ※レンダー内に二つviewは不自然でエラー原因

--- app/Livewire/CartItemNum.php
+++ app/Livewire/CartItemNum.php
@@ -3,21 +3,20 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use App\Models\Cart;
 
 class CartItemNum extends Component
 {
-    public $count = 0;
+    public $cartItem;
 
-    public function update($count)
+    public function update($cartItem)
     {
-        $this->$count++;
-        $this->$count--;
+        $this->$cartItem++;
+        $this->$cartItem--;
     }
 
     public function render()
     {
-        return view('livewire.increment');
-        return view('livewire.decrement');
+
+        return view('livewire.CartItemNum');
     }
 }
