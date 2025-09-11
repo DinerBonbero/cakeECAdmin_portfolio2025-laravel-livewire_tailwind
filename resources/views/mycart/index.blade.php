dfc2c5c3a79181ebbf5ec87bdb040abc48b1903a
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-11 15:23:59 +0900

    コンポーネント呼び出し

--- resources/views/mycart/index.blade.php
+++ resources/views/mycart/index.blade.php
@@ -20,12 +20,23 @@
                     <img class="w-1/4" src="{{ asset('/images/' . $cartItem->item->image) }}">
                     <div class="flex flex-col ml-25 w-full text-left">
                         <span class="mt-2">{{ $cartItem->item->name }}</span>
-                        <div class="mt-5">
-                            <button wire:click="increment({{ $cartItem->item_num }})" class="text-gray-400 text-2xl">+</button>
-                            <input readonly type="number" id="item_num" name="item_num"
-                                value="{{ old('item_num', $cartItem->item_num) }}" min="1" max="10" step="1"
-                                class="w-1/3 bg-white border-1 border-solid border-gray-200 rounded-sm text-center hide-spin">
-                            <button wire:click="decrement({{ $cartItem->item_num }})" class="text-gray-400 text-2xl">-</button>
+                        <div class="w-full">
+                            <div class="mt-5 inline-block relative w-4/11">
+                                <livewire:decrement>
+                                {{-- <button wire:click="decrement({{ $cartItem->item_id }})"
+                                    class="text-gray-400 text-2xl absolute left-2">
+                                    -
+                                </button> --}}
+                                <input readonly type="number" id="item_num" name="item_num"
+                                    value="{{ old('item_num', $cartItem->item_num) }}" min="1" max="10"
+                                    step="1"
+                                    class="w-full bg-white border-3 border-solid border-gray-300 rounded-sm text-center hide-spin">
+                                {{-- <button wire:click="increment({{ $cartItem->item_id }})"
+                                    class="text-gray-400 text-2xl absolute right-2">
+                                    +
+                                </button> --}}
+                                <livewire:increment>
+                            </div>
                             <label for="item_num" class="ml-10">個</label>
                         </div>
                     </div>
