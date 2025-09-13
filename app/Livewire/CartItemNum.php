859235bea0df08be96c3b6e3ecadb1968116a859
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-13 11:47:08 +0900

    数量変更のupdate処理

--- app/Livewire/CartItemNum.php
+++ app/Livewire/CartItemNum.php
@@ -3,15 +3,38 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 
 class CartItemNum extends Component
 {
+
     public $cartItem;
 
-    public function update($cartItem)
+    public function update($CartId, $delta)
     {
-        $this->$cartItem++;
-        $this->$cartItem--;
+        if ($delta == '+') {
+
+            $cart = Cart::where('user_id', Auth::id())->where('id', $CartId)->first();
+            dd($cart);
+            exit();
+
+            $cart->update([
+                'item_num' => $cart->item_num + 1,
+            ]);
+
+        } elseif ($delta == '-') {
+
+            $cart = Cart::where('user_id', Auth::id())->where('id', $CartId)->first();
+            dd($cart);
+            exit();
+
+            $cart->update([
+                'item_num' => $cart->item_num - 1,
+            ]);
+         }
+
+
     }
 
     public function render()
