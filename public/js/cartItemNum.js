56ff7f0993ed406e1eb3e5291fd2dfb116bc5a6b
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-15 23:53:31 +0900

    増減ボタンの動的処理却下のためコメントアウト

--- public/js/cartItemNum.js
+++ public/js/cartItemNum.js
@@ -1,15 +1,50 @@
-document.getElementById("increment").addEventListener("click", function () {
-    let itemNumInput = document.getElementById("item_num");// 数量変更欄(input要素)を変数に代入
-    let currentValue = parseInt(itemNumInput.value);// 数量変更欄の値を数値に変換して変数に代入
-    if (currentValue < 10) { // 最低数量を1に制限
-        itemNumInput.value = currentValue + 1;
-    }
-});
+// const elements = document.querySelectorAll(".cart-item-num");
 
-document.getElementById("decrement").addEventListener("click", function () {
-    let itemNumInput = document.getElementById("item_num");
-    let currentValue = parseInt(itemNumInput.value);
-    if (currentValue > 1) { // 最低数量を1に制限
-        itemNumInput.value = currentValue - 1;
-    }
-});
+// let cartItemNum = document.querySelectorAll(".cart-item-num");
+// let btn = document.querySelector('.btn');
+
+// document.querySelectorAll(".cart-item-num").forEach(function () {
+//     addEventListener("click", function () {
+//     let itemNumInput = document.getElementById("item_num");// 数量変更欄(input要素)を変数に代入
+//     let currentValue = parseInt(itemNumInput.value);// 数量変更欄の値を数値に変換して変数に代入
+//     if (currentValue <= 9) { // 最低数量を1に制限
+//         itemNumInput.value = currentValue + 1;
+//     }
+// });
+// });
+
+// .addEventListener("click", function () {
+//     let itemNumInput = document.getElementById("item_num");// 数量変更欄(input要素)を変数に代入
+//     let currentValue = parseInt(itemNumInput.value);// 数量変更欄の値を数値に変換して変数に代入
+//     if (currentValue <= 9) { // 最低数量を1に制限
+//         itemNumInput.value = currentValue + 1;
+//     }
+// });
+
+
+// box.forEach((element) => {
+//  element.innerHTML = ‘boxです’;
+// });
+// const btn = document.querySelector(‘.btn’);
+// 変数.addEventListener(‘click’, () => {
+// ここに処理を書く
+// });
+
+
+
+// document.getElementById("increment").addEventListener("click", function () {
+//     alert( 画面に表示させたい値 )
+//     let itemNumInput = document.getElementById("item_num");// 数量変更欄(input要素)を変数に代入
+//     let currentValue = parseInt(itemNumInput.value);// 数量変更欄の値を数値に変換して変数に代入
+//     if (currentValue <= 9) { // 最低数量を1に制限
+//         itemNumInput.value = currentValue + 1;
+//     }
+// });
+
+// document.getElementById("decrement").addEventListener("click", function () {
+//     let itemNumInput = document.getElementById("item_num");
+//     let currentValue = parseInt(itemNumInput.value);
+//     if (currentValue >= 2) { // 最低数量を1に制限
+//         itemNumInput.value = currentValue - 1;
+//     }
+// });
