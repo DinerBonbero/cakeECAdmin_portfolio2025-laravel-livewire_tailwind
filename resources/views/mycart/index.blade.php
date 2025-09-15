ab38bc8a7421119cd98c3a084cd812cb9e47b8e8
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-15 23:46:31 +0900

    動的処理却下のためレイアウト変更

--- resources/views/mycart/index.blade.php
+++ resources/views/mycart/index.blade.php
@@ -18,23 +18,23 @@
                 @endphp
                 <div class="flex mt-3">
                     <img class="w-1/4" src="{{ asset('/images/' . $cartItem->item->image) }}">
-                    <div class="flex flex-col ml-25 w-full text-left">
+                    <div class="flex flex-col ml-20 w-full text-left">
                         <span class="mt-2">{{ $cartItem->item->name }}</span>
                         <div class="w-full">
-                            <div class="mt-5 inline-block relative w-4/11">
-                                <button class="text-gray-400 text-2xl absolute left-2">
-                                    -
-                                </button>
-                                <input readonly type="number" id="item_num" name="item_num"
-                                    value="{{ old('item_num', $cartItem->item_num) }}" min="1" max="10"
-                                    step="1"
-                                    class="w-full bg-white border-3 border-solid border-gray-300 rounded-sm text-center hide-spin">
-                                <button class="text-gray-400 text-2xl absolute right-2">
-                                    +
-                                </button>
+                            <div class="mt-5 w-full">
+                                <form action="{{ route('mycart_item.update', $cartItem) }}" method="POST">
+                                    @method('PATCH')
+                                    @csrf
+                                    <input type="number" id="item_num" name="item_num"
+                                        value="{{ old('item_num', $cartItem->item_num) }}" min="1" max="10"
+                                        step="1"
+                                        class="w-3/11 bg-white border-3 border-solid border-gray-300 rounded-sm text-center">
+                                    <label for="item_num" class="mx-1 inline">個</label>
+                                    <button class="inline mx-1 py-1 w-4/11 bg-lime-500 text-white rounded-xl border-3 border-solid border-gray-200">
+                                        数量更新
+                                    </button>
+                                </form>
                             </div>
-                            {{-- <livewire:CartItemNum :cartItem="$cartItem" /> --}}
-                            <label for="item_num" class="ml-10">個</label>
                         </div>
                     </div>
                     <div class="flex flex-col w-full">
@@ -46,19 +46,22 @@
                                 削除
                             </button>
                         </form>
-                        <span class="mt-4">{{ '小計(税込み)' . ' ' . number_format($subtotal) . '円' }}</span>
                     </div>
                 </div>
-                <div class="py-1 text-rose-500 border-b-3 border-solid border-gray-200">
-                    <span class="mr-30">エラーメッセージ</span>
+                <div class="border-b-3 border-solid border-gray-200 flex justify-between">
+                    <span></span>
+                    <span class="text-rose-500">エラーメッセージ</span>
+                    <span>{{ '小計(税込み)' . ' ' . number_format($subtotal) . '円' }}</span>
                 </div>
             @endforeach
             <div class="mb-7 border-b-3 border-solid border-gray-200 text-right text-xl">
                 {{ '合計(税込み)' . '　' . number_format($total) . '円' }}
             </div>
-
             <div class="text-center pb-10 w-full">
                 <x-button.return message="戻る" href="{{ url()->previous() }}" />
+                <a href="{{ route('order.confirm') }}" class="py-1 px-10 bg-[#7cc7f4] text-white rounded-lg w-full border-3 border-solid border-gray-200">
+                    注文
+                </a>
             </div>
         </div>
     </x-layouts.app.header>
