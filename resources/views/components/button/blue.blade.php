2c12558fefc9e6025ada0b117788c55b9572771d
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-16 21:37:34 +0900

    ブルーボタンのコンポーネントを<x->で扱えるように修正

--- resources/views/components/button/blue.blade.php
+++ resources/views/components/button/blue.blade.php
@@ -1,3 +1,3 @@
-<button class="py-1 text-white rounded-xl w-full border-3 border-solid border-gray-200">
-    {{ $slot }}
+<button class="bg-[#7cc7f4] py-1 w-full text-white rounded-xl border-3 border-solid border-gray-200">
+    {{ $message }}
 </button>
