6996a728339fc2b1015581d3411c7cbb1427674a
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-16 17:30:11 +0900

    注文確認画面表示のconfirm()メソッド記述中

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+    public function confirm()
+    {
+        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
+        //latest「最新の」
+        //dd($items);
+        // exit();
+        return view('order.confirm', compact('cartItems'));
+    }
 }
