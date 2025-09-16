7edec5b9f06951b0f325d7d0e7b6ff2bf327142e
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-16 21:42:12 +0900

    レイアウトの不要な部分を削除：記述中

--- resources/views/order/confirm.blade.php
+++ resources/views/order/confirm.blade.php
@@ -10,7 +10,7 @@
         <div class="py-3">
             <span class="text-2xl">カート内の商品</span>
         </div>
-        <div class="w-4/7 mx-auto">
+        <div class="w-5/8 mx-auto">
             @foreach ($cartItems as $cartItem)
                 @php
                     $subtotal = $cartItem->item->price * $cartItem->item_num;
@@ -23,11 +23,7 @@
                         <div class="w-full">
                             <div class="mt-5 w-full text-center">
                                 <span class="w-3/11">{{ $cartItem->item_num }}</span>
-                                {{-- formのnameが同一によりエラーメッセージが一つにまとまってしまうため、nameを配列に変更item_num[{{ $cartItem->id }}] --}}
                                 <span for="item_num" class="mx-1 inline">個</span>
-                                {{-- <button class="inline mx-1 py-1 w-4/11 bg-lime-500 text-white rounded-xl border-3 border-solid border-gray-200">
-                                        数量更新
-                                    </button> --}}
                                 </form>
                             </div>
                         </div>
@@ -38,23 +34,49 @@
                 </div>
                 <div class="border-b-3 border-solid border-gray-200 flex justify-between">
                     <span></span>
-                    {{-- エラーメッセージの表示 --}}
-                    {{-- 配列に変更したため、エラーメッセージの表示も変更 @error('item_num.' . $cartItem->id)ドットを付けてitem_num[{{ $cartItem->id }}]のエラー呼び出し --}}
-                    @error('item_num.' . $cartItem->id)
-                        <span class="text-rose-500">{{ $message }}</span>
-                    @enderror
                     <span>{{ '小計(税込み)' . ' ' . number_format($subtotal) . '円' }}</span>
                 </div>
             @endforeach
-            <div class="mb-7 border-b-3 border-solid border-gray-200 text-right text-xl">
+            <div class="border-b-3 border-solid border-gray-200 text-right text-lg">
                 {{ '合計(税込み)' . '　' . number_format($total) . '円' }}
             </div>
-            <div class="text-center pb-10 w-full">
-                <x-button.return message="戻る" href="{{ url()->previous() }}" />
-                <a href="{{ route('order.confirm') }}"
-                    class="ml-2 py-1 px-10 bg-[#7cc7f4] text-white rounded-lg w-full border-3 border-solid border-gray-200">
-                    注文
-                </a>
+            <div class="text-right mt-2">
+                <x-button.brown-link message="カートの修正" href="{{ route('mycart_item.index') }}" />
+            </div>
+            <div class="py-1">
+                <span class="text-2xl">送り先</span>
+            </div>
+            <div class="flex border-b-3 border-solid border-gray-200">
+                <span>お名前</span>
+                <span>{{ $userInfo->last_name }}{{ $userInfo->first_name }}</span>
+            </div>
+            <div class="flex border-b-3 border-solid border-gray-200">
+                <span>ご住所</span>
+                <div class="flex border-b-3 border-solid border-gray-200 flex-col ml-3">
+                    <span>{{ '〒' . $userInfo->postal_code }}</span>
+                    <div>
+                        <span>{{ $userInfo->prefecture }}</span>
+                        <span>{{ $userInfo->street_address }}</span>
+                        <span>{{ $userInfo->address_detail }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="flex border-b-3 border-solid border-gray-200">
+                <span>電話番号</span>
+                <span>{{ $userInfo->phone_number }}</span>
+            </div>
+            <div class="flex border-b-3 border-solid border-gray-200">
+                <span>メールアドレス</span>
+                <span>{{ $mail }}</span>
+            </div>
+            <div class="text-right mt-2">
+                <x-button.brown-link message="送り先修正" href="{{ route('mycart_item.index') }}" />
+            </div>
+            <div class="w-1/5 mx-auto mt-10 mb-5">
+                <form action="{{ route('order.store') }}" method="POST">
+                    @csrf
+                    <x-button.blue message="注文確定" />
+                </form>
             </div>
         </div>
     </x-layouts.app.header>
