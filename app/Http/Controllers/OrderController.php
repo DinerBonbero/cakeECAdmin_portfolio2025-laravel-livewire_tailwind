016c297c2cd3c8397b1a894b89d7584403c2c133
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-16 21:36:36 +0900

    storeメソッド記述中

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserInfo;
+use App\Models\User;
 
 class OrderController extends Controller
 {
@@ -14,6 +16,27 @@ class OrderController extends Controller
         //latest「最新の」
         //dd($items);
         // exit();
-        return view('order.confirm', compact('cartItems'));
+        $userInfo = UserInfo::where('user_id', Auth::id())->first();
+        $mail = User::where('id', Auth::id())->value('email');
+        // dd($mail);
+        // exit();
+
+        return view('order.confirm', compact('cartItems', 'userInfo', 'mail'));
+    }
+
+    public function store()
+    {
+        // $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
+
+        // foreach ($cartItems as $cartItem) {
+        //     $item = $cartItem->item;
+        //     $item->update([
+        //         'is_pending' => 1,
+        //     ]);
+        // }
+
+        // Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('items.index');
     }
 }
