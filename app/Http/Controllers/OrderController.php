289fc8114313987392ce0cd4f642235e542dc568
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-18 23:34:29 +0900

    トランザクション記述中

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Cart;
-use Illuminate\Support\Facades\Auth;
-use App\Models\UserInfo;
+use App\Models\Order;
 use App\Models\User;
+use App\Models\UserInfo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
     public function confirm()
     {
-        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
-        //latest「最新の」
-        //dd($items);
+        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); // テーブルが複数か単数か気を付ける
+        // latest「最新の」
+        // dd($items);
         // exit();
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
         $mail = User::where('id', Auth::id())->value('email');
@@ -26,16 +28,20 @@ class OrderController extends Controller
 
     public function store()
     {
-        // $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
+        try {
 
-        // foreach ($cartItems as $cartItem) {
-        //     $item = $cartItem->item;
-        //     $item->update([
-        //         'is_pending' => 1,
-        //     ]);
-        // }
+            DB::transaction(function () {
 
-        // Cart::where('user_id', Auth::id())->delete();
+                Cart::where('user_id', Auth::id())->delete();
+
+                Order::create([
+                    'login_id' => login_id,
+                    'password' => password,
+                ]);
+            }, 5);
+        } catch (\Exception $e) {
+            Log::error('購入時のエラー');
+        }
 
         return redirect()->route('order.thank_you');
     }
