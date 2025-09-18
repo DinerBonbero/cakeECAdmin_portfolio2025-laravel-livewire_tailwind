6f3558dc7a2181e2369a71addd7d4ca259ffb7e8
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-18 14:33:50 +0900

    thankYou()メソッド記述：サンクスページの表示メソッド

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Cart;
-use Illuminate\Support\Facades\Auth;
-use App\Models\UserInfo;
 use App\Models\User;
+use App\Models\UserInfo;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function confirm()
     {
-        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
-        //latest「最新の」
-        //dd($items);
+        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); // テーブルが複数か単数か気を付ける
+        // latest「最新の」
+        // dd($items);
         // exit();
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
         $mail = User::where('id', Auth::id())->value('email');
@@ -37,6 +36,11 @@ class OrderController extends Controller
 
         // Cart::where('user_id', Auth::id())->delete();
 
-        return redirect()->route('items.index');
+        return redirect()->route('order.thank_you');
+    }
+
+    public function thankYou()
+    {
+        return view('order.thank_you');
     }
 }
