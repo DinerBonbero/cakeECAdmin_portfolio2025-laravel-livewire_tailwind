616bdd3d88fb6f7608dc4274e767841bfb2cd72b
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-18 14:35:21 +0900

    use Requestの記述

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Cart;
-use App\Models\User;
-use App\Models\UserInfo;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserInfo;
+use App\Models\User;
 
 class OrderController extends Controller
 {
     public function confirm()
     {
-        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); // テーブルが複数か単数か気を付ける
-        // latest「最新の」
-        // dd($items);
+        $cartItems = Cart::with('item')->whereRelation('item', 'is_pending', 0)->where('user_id', Auth::id())->latest('id')->get(); //テーブルが複数か単数か気を付ける
+        //latest「最新の」
+        //dd($items);
         // exit();
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
         $mail = User::where('id', Auth::id())->value('email');
