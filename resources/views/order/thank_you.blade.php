37399d9b2d172003aded6a44d26b3a9e2c9cabe8
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-22 15:55:38 +0900

    誤字修正

--- resources/views/order/thank_you.blade.php
+++ resources/views/order/thank_you.blade.php
@@ -5,16 +5,16 @@
 @php
     $total = 0;
     foreach ($orders->order_details as $order_details) {
-            $total += $order_details->item->price*$order_details->item_num;
+        $total += $order_details->item->price * $order_details->item_num;
     }
 @endphp
 @can('user')
     <x-layouts.app.header>
         <div class="w-5/8 mx-auto">
-            <div class="p-10 my-10 bg-white font-serif text-gray-800 border-3 border-solid border-gray-200">
+            <div class="p-10 my-10 bg-white font-serif text-gray-800 border-1 border-solid border-gray-200">
                 <p class="mb-3 text-2xl">購入完了しました。</p>
                 <p>弊社のお菓子をお買い求め頂きありがとうございます。</p>
-                <p>お支払金額は<span class="font-mono">{{ $total }}</span>円です。</p>
+                <p>お支い払金額は<span class="font-mono">{{ $total }}</span>円です。</p>
             </div>
             <x-button.brown-link message="ホームに戻る" href="{{ route('items.index') }}" />
         </div>
