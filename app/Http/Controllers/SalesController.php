adaa79c4765858db3916f92bea0b32625a902643
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-25 15:18:50 +0900

    リレーションの際にUserの望まないカラム(パスワードなど)が含まれていたためfix

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    public function history(){
-        return view('sales.history');
+    public function history()
+    {
+
+        $saleHistories = Order::with('order_details.item')->with(['user' => function ($query) {
+            $query->select('id');
+        }, 'user.user_info'])->latest('date')->paginate(3);
+
+        // dd($saleHistories);
+        // exit();
+        return  view('sales.history', compact('saleHistories'));
     }
 }
