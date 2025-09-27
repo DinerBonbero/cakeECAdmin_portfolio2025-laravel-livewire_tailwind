6bfe0445428346bed07557544d718633592d4e56
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-27 10:39:47 +0900

    不要なファイル削除

--- resources/views/livewire/decrement.blade.php
+++
@@ -1,3 +0,0 @@
-<button wire:click="update" class="text-gray-400 text-2xl absolute left-2">
-    -
-</button>
--- resources/views/livewire/increment.blade.php
+++
@@ -1,3 +0,0 @@
-<button wire:click="update" class="text-gray-400 text-2xl absolute right-2">
-    +
-</button>
