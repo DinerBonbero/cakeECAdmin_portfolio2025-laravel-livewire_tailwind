27b6a7ce6fbe85140345069d72bb49b3d60f39a2
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-09-28 23:52:05 +0900

    検索条件記述中

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -9,8 +9,24 @@ use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    public function history()
+    public function history(Request $request)
     {
+        dd($request);
+        exit();
+
+        // un_shipped 検索のname
+        // shipped
+        // start_date
+        // end_date
+        // purchaser_name
+
+        // if(!empty($keyword)) {//$keyword　が空ではない場合、検索処理を実行します
+        //     $companies->where('company_name', 'LIKE', "%{$keyword}%")
+        //     ->orwhereHas('products', function ($query) use ($keyword) {
+        //         $query->where('product_name', 'LIKE', "%{$keyword}%");
+        //     })->get();
+
+        // }
 
         $saleHistories = Order::with('order_details.item')->with(['user' => function ($query) {
             $query->select('id');
