45af824d054d2c719f6c4312cf573dad1a17d996
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-01 19:52:54 +0900

    検索のif条件をissetに統一

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -17,19 +17,19 @@ class SalesController extends Controller
     public function history(Request $request)
     {
 
-        // if ($request->session()->exists(['un_shipped', 'shipped', 'start_date', 'end_date', 'purchaser_name'])) {
-        //     $request->session()->forget(['un_shipped', 'shipped', 'start_date', 'end_date', 'purchaser_name']);
-        // }
+        if ($request->session()->exists(['un_shipped', 'shipped', 'start_date', 'end_date', 'purchaser_name'])) {
+            $request->session()->forget(['un_shipped', 'shipped', 'start_date', 'end_date', 'purchaser_name']);
+        }
         // dd($request);
         // exit();
 
         if ($request->hasAny(['un_shipped', 'shipped', 'start_date', 'end_date', 'purchaser_name'])) {
-            //★バリデーション記述必要！！！！
+
             $validated = $request->validate([
                 'un_shipped' => Rule::in('0'),
                 'shipped' => Rule::in('1'),
-                'start_date' =>  [Rule::date()->format('Y-m-d'),'nullable'],
-                'end_date' => [Rule::date()->format('Y-m-d'),'nullable'],
+                'start_date' =>  [Rule::date()->format('Y-m-d'), 'nullable'],
+                'end_date' => [Rule::date()->format('Y-m-d'), 'nullable'],
                 'purchaser_name' => 'max:30'
             ], [
                 'un_shipped.in' => 'この値は無効です、選択肢から選んでください。',
@@ -39,56 +39,59 @@ class SalesController extends Controller
                 'purchaser_name.max' => '入力文字は30文字以上で入力してください。',
             ]);
 
+            // dd($validated);
+            // exit();
+
             $query = Order::query();
 
-            if ('0' === $request->input('un_shipped') && !empty($request->input('shipped'))) {
+            if (isset($validated['un_shipped']) && isset($validated['shipped'])) {
 
-                $query->where('is_shipped', $request->un_shipped)
-                    ->orWhere('is_shipped', $request->shipped);
+                $query->where('is_shipped', $validated['un_shipped'])
+                    ->orWhere('is_shipped', $validated['shipped']);
 
-                // $request->session()->put('un_shipped', $request->input('un_shipped'));
-                // $request->session()->put('shipped', $request->input('shipped'));
-            } elseif ('0' === $request->input('un_shipped')) {
+                $request->session()->put('un_shipped', $validated['un_shipped']);
+                $request->session()->put('shipped', $validated['shipped']);
+            } elseif (isset($validated['un_shipped'])) {
 
-                $query->where('is_shipped', $request->un_shipped);
+                $query->where('is_shipped', $validated['un_shipped']);
 
-                // $request->session()->put('un_shipped', $request->input('un_shipped'));
-            } elseif (!empty($request->input('shipped'))) {
+                $request->session()->put('un_shipped', $validated['un_shipped']);
+            } elseif (isset($validated['shipped'])) {
 
-                $query->where('is_shipped', $request->shipped);
+                $query->where('is_shipped', $validated['shipped']);
 
-                // $request->session()->put('shipped', $request->input('shipped'));
+                $request->session()->put('shipped', $validated['shipped']);
             } else {
             }
 
-            if (!empty($request->input('start_date'))) {
+            if (isset($validated['start_date'])) {
 
-                $query->whereDate('date', '>=', $request->start_date);
+                $query->whereDate('date', '>=', $validated['start_date']);
 
-                // $request->session()->put('start_date', $request->input('start_date'));
+                $request->session()->put('start_date', $validated['start_date']);
             }
 
-            if (!empty($request->input('end_date'))) {
+            if (isset($validated['end_date'])) {
 
-                $query->whereDate('date', '<=', $request->end_date);
+                $query->whereDate('date', '<=', $validated['end_date']);
 
-                // $request->session()->put('end_date', $request->input('end_date'));
+                $request->session()->put('end_date', $validated['end_date']);
             }
 
-            if (!empty($request->input('purchaser_name'))) {
-                $query->whereRelation('user.user_info', 'last_name', 'like', "%{$request->purchaser_name}%")
-                    ->orWhereRelation('user.user_info', 'first_name', 'like', "%{$request->purchaser_name}%");
+            if (isset($validated['purchaser_name'])) {
+                $query->whereRelation('user.user_info', 'last_name', 'like', "%{$validated['purchaser_name']}%")
+                    ->orWhereRelation('user.user_info', 'first_name', 'like', "%{$validated['purchaser_name']}%");
 
                 // $query->where(DB::raw('CONCAT(user.user_info.last_name, user.user_info.first_name)'), 'like', "%{$request->purchaser_name}%");
 
-                // $request->session()->put('purchaser_name', $request->input('purchaser_name'));
+                $request->session()->put('purchaser_name', $validated['purchaser_name']);
             }
 
             $saleHistories = $query->with('order_details.item')->with(['user' => function ($query) {
                 $query->select('id');
             }, 'user.user_info'])->latest('date')->paginate(3); //->withQueryString()検索結果のがペジネーションに反映されるように->withQueryString()
 
-            return  view('sales.history', compact('saleHistories'));
+            return  view('sales.history', compact('saleHistories', ));
         } else {
             $saleHistories = Order::with('order_details.item')->with(['user' => function ($query) {
                 $query->select('id');
