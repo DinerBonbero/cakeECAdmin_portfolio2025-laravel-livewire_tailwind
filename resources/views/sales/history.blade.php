ddf3f94ff3839533304384f7f4d688e4ca302f19
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-02 19:56:47 +0900

    @ifから三項演算子、null演算子に変更：HTML属性内の@ディレクティブによりエラー空白が含まれてinput dateが表示されなかった

--- resources/views/sales/history.blade.php
+++ resources/views/sales/history.blade.php
@@ -1,10 +1,9 @@
-@php
-    if (session('un_shipped')) {
-        var_dump(session('un_shipped'));
+{{-- @php
+    if (isset($validatedSearchInputs)) {
+        dd($validatedSearchInputs);
         exit();
     }
-
-@endphp
+@endphp --}}
 
 @can('is_admin')
     <x-layouts.app.header>
@@ -23,13 +22,15 @@
                             <div>
                                 <input type="checkbox" name="un_shipped" value="0"
                                     class="bg-white border-1 border-solid border-gray-300 rounded-sm"
-                                    {{ old('un_shipped') === '0' ? 'checked' : '' }}>
+                                    {{ old('un_shipped') === '0' ? 'checked' : '' }}
+                                    {{ isset($validatedSearchInputs['un_shipped']) ? 'checked' : '' }}>
                                 未発送
                             </div>
                             <div class="pl-5">
                                 <input type="checkbox" name="shipped" value="1"
                                     class="bg-white border-1 border-solid border-gray-300 rounded-sm"
-                                    {{ old('shipped') === '1' ? 'checked' : '' }}>
+                                    {{ old('shipped') === '1' ? 'checked' : '' }}
+                                    {{ isset($validatedSearchInputs['shipped']) ? 'checked' : '' }}>
                                 発送済み
                             </div>
                         </div>
@@ -49,11 +50,11 @@
                         <div>
                             <input type="date" name="start_date"
                                 class="bg-white border-1 border-solid border-gray-300 rounded-sm"
-                                value="{{ old('start_date') }}">
+                                value="{{ old('start_date') }}{{ $validatedSearchInputs['start_date'] ?? '' }}">
                             <span>～</span>
                             <input type="date" name="end_date"
                                 class="bg-white border-1 border-solid border-gray-300 rounded-sm"
-                                value="{{ old('end_date') }}">
+                                value="{{ old('end_date') }}{{ $validatedSearchInputs['end_date'] ?? '' }}">
                         </div>
                         @error('start_date')
                             <div class="text-left">
@@ -64,18 +65,13 @@
                             <div class="text-left">
                                 <span class="text-red-500">{{ $message }}</span>
                             </div>
-                        @enderror  
+                        @enderror
                     </div>
                     <div class="flex flex-col">
                         <span>購入者名</span>
                         <input type="text" name="purchaser_name"
                             class="bg-white border-1 border-solid border-gray-300 rounded-sm"
-                            value="{{ old('purchaser_name') }}">
-                        @error('purchaser_name')
-                            <div class="text-left">
-                                <span class="text-red-500">{{ $message }}</span>
-                            </div>
-                        @enderror 
+                            value="{{ $purchaserName ?? '' }}">
                     </div>
                     <div>
                         <button class="bg-[#7cc7f4] p-1 px-2 text-white rounded-md border-3 border-solid border-gray-200">
