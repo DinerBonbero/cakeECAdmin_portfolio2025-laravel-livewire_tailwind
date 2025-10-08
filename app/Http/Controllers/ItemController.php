4b8c2ff71456ac76d18d99fe6047bd37d7ac4db9
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-08 18:12:29 +0900

    storeメソッドに遷移先(商品一覧画面)記述

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -31,9 +31,10 @@ class ItemController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        // Request $request
+        return redirect()->route('items.index');
     }
 
     /**
