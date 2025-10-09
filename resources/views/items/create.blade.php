c27ac6e496af41f55ce1f167e084bf5c6ca5eb80
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-09 15:43:56 +0900

    id,for,name属性名をカラム名と統一 表示テキスト「添付ファイル」→「添付画像」に修正

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -13,28 +13,28 @@
             @csrf
             <div class="flex">
                 <div class="text-center w-3/5 relative mb-2">
-                    <span class="absolute top-11 left-0">添付ファイル</span>
+                    <span class="absolute top-11 left-0">添付画像</span>
                     {{-- <label for="file" class="absolute bottom-0 left-0 w-1/3 py-5 px-5 bg-white border-1 border-solid border-gray-200 rounded-xl">
                         ファイルの選択
                     </label> --}}
-                    <input type="file" name="" id="file" class="absolute bottom-0 left-0 w-full file:py-5 file:px-5 file:mr-10 file:bg-white file:border-1 file:border-solid file:border-gray-200 file:rounded-xl hover:file:bg-gray-100">
+                    <input type="file" name="image" accept="image/png, image/jpeg" class="absolute bottom-0 left-0 w-full file:py-5 file:px-5 file:mr-10 file:bg-white file:border-1 file:border-solid file:border-gray-200 file:rounded-xl hover:file:bg-gray-100">
                     {{-- <span class="absolute bottom-5 right-20">ファイルが選択されていません</span> --}}
                 </div>
                 <div class="flex flex-col text-left ml-5 text-lg w-2/5">
-                    <label for="item_name">
+                    <label for="name">
                         商品名
                     </label>
                     <div class="w-full">
-                        <input type="text" name="" id="item_name"
+                        <input type="text" name="name" id="name"
                             class="w-4/5 bg-white border-1 border-solid border-gray-200 rounded-sm"
                             placeholder="ショートケーキ">
                     </div>
                     <span class="text-red-500 text-base">エラーメッセージ</span>
-                    <label class="mt-2" for="item_price">
+                    <label class="mt-2" for="price">
                         金額税込み
                     </label>
                     <div class="w-full">
-                        <input type="number" name="" id="item_price"
+                        <input type="number" name="price" id="price"
                             class="w-4/5 bg-white border-1 border-solid border-gray-200 rounded-sm mr-2"
                             placeholder="480" min="300" max="10000" step="10">円
                     </div>
@@ -45,8 +45,8 @@
                 <span class="text-red-500">エラーメッセージ</span>
             </div>
             <div class="text-left pt-7 pb-10 w-full">
-                <label class="pl-2" for="item_description">商品説明</label>
-                <textarea name="" id="item_description" class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
+                <label class="pl-2" for="description">商品説明</label>
+                <textarea name="description" id="description" class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
                     placeholder="ほのかな酸味の苺とあっさりしてコクのある生クリームを使用した、甘すぎないショートケーキです。"></textarea>
                 <span class="text-red-500">エラーメッセージ</span>
                 <div class="text-center pt-5">
