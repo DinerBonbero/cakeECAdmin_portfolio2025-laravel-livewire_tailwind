f7834aa15c65b78f91b08c0a636af9e1bd0fa76b
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-10 11:34:11 +0900

    送信した画像が認識されなかったのでfix 原因：enctype="multipart/form-data"を記述していなかった。
    入力欄にエラー時の入力値を保持するold()記述

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -9,24 +9,20 @@
         <span class="text-emerald-500">※登録された商品の画像は1:2の縦横比で出力しますのでそれを考慮したうえでアップロードしてください</span>
     </div>
     <div class="w-5/9 mx-auto">
-        <form action="{{ route('items.store') }}" method="POST">
+        <form action="{{ route('items.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="flex">
                 <div class="text-center w-3/5 relative mb-2">
                     <span class="absolute top-2 left-0">添付画像</span>
-                    {{-- <label for="file" class="absolute bottom-0 left-0 w-1/3 py-5 px-5 bg-white border-1 border-solid border-gray-200 rounded-xl">
-                        ファイルの選択
-                    </label> --}}
-                    <input type="file" name="image" accept="image/png, image/jpeg"
+                    <input type="file" name="image" accept="image/png, image/jpeg" value="{{ old('image') }}"
                         class="absolute bottom-0 left-0 w-full file:py-5 file:px-5 file:mr-10 file:bg-white file:border-1 file:border-solid file:border-gray-200 file:rounded-xl hover:file:bg-gray-100">
-                    {{-- <span class="absolute bottom-5 right-20">ファイルが選択されていません</span> --}}
                 </div>
                 <div class="flex flex-col text-left ml-5 text-lg w-2/5">
                     <label for="name">
                         商品名
                     </label>
                     <div class="w-full">
-                        <input type="text" name="name" id="name"
+                        <input type="text" name="name" id="name" value="{{ old('name') }}"
                             class="w-4/5 bg-white border-1 border-solid border-gray-200 rounded-sm"
                             placeholder="ショートケーキ">
                     </div>
@@ -37,7 +33,7 @@
                         金額税込み
                     </label>
                     <div class="w-full">
-                        <input type="number" name="price" id="price"
+                        <input type="number" name="price" id="price" value="{{ old('price') }}"
                             class="w-4/5 bg-white border-1 border-solid border-gray-200 rounded-sm mr-2"
                             placeholder="480" min="300" max="10000" step="10">円
                     </div>
@@ -53,8 +49,7 @@
             </div>
             <div class="text-left pt-7 pb-10 w-full">
                 <label class="pl-2" for="description">商品説明</label>
-                <textarea name="description" id="description" class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
-                    placeholder="ほのかな酸味の苺とあっさりしてコクのある生クリームを使用した、甘すぎないショートケーキです。"></textarea>
+                <textarea name="description" id="description" class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full" placeholder="ほのかな酸味の苺とあっさりしてコクのある生クリームを使用した、甘すぎないショートケーキです。">{{ old('description') }}</textarea>
                 @error('description')
                     <span class="text-red-500 text-base">{{ $message }}</span>
                 @enderror
