6399154caaedf1023b1d0894ba54f0ddac7f4bd9
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-14 14:50:11 +0900

    一般ユーザーの権限チェックミドルウェアの記述

--- app/Http/Middleware/UserCheckMiddleware.php
+++ app/Http/Middleware/UserCheckMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class UserCheckMiddleware
 {
@@ -15,6 +17,14 @@ class UserCheckMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->is_admin === 0) {
+
+            //ログインユーザーのis_adminカラムが1、つまり一般ユーザーなら次に進める
+            return $next($request);
+        } else {
+
+            //ログインユーザーのis_adminカラムが0、つまり一般ユーザーであれば前のページに戻す
+            return redirect()->route('items.index');
+        }
     }
 }
