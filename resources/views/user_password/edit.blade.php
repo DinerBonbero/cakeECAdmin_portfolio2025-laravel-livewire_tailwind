15e02383901560adc9ff9560b5cf307f6c642d4d
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-16 13:42:00 +0900

    name属性の変更 不要なコードの削除 書体をserifに修正

--- resources/views/user_password/edit.blade.php
+++ resources/views/user_password/edit.blade.php
@@ -2,28 +2,26 @@
     <div class="w-4/11 mx-auto">
         <div class="flex mb-7">
             <div class="ml-5 w-full">
-                <p class="my-15 text-lg">パスワードの変更</p>
+                <p class="my-15 text-lg serif">パスワードの変更</p>
                 <form action="{{ route('user_password.update') }}" method="POST">
                     @csrf
                     @method('PATCH')
                     <div class="text-left text-base">
                         <div class="mb-10">
-                            <label for="phone_number" class="block">新しいパスワードを入力してください</label>
-                            <input type="tel" id="phone_number" name="phone_number"
-                                class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
-                                placeholder="090-1234-5678">
-                            @error('phone_number')
+                            <label for="password" class="block serif">新しいパスワードを入力してください</label>
+                            <input type="tel" id="password" name="password"
+                                class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full">
+                            @error('password')
                                 <span class="block text-rose-500">
                                     {{ $message }}
                                 </span>
                             @enderror
                         </div>
                         <div class="mb-10">
-                            <label for="postal_code" class="block">新しいパスワードを再入力してください</label>
-                            <input type="text" id="postal_code" name="postal_code"
-                                class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full"
-                                placeholder="012-3456">
-                            @error('postal_code')
+                            <label for="password_confirmation" class="block serif">新しいパスワードを再入力してください</label>
+                            <input type="text" id="password_confirmation" name="password_confirmation"
+                                class="bg-white border-1 border-solid border-gray-200 rounded-sm w-full">
+                            @error('password_confirmation')
                                 <span class="block text-rose-500">
                                     {{ $message }}
                                 </span>
