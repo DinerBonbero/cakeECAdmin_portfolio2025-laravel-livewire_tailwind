17a03a4520baf1956f0c16892215cb0cb4428ec0
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-17 00:21:31 +0900

    仮：バリデーションルールとメッセージの記述中

--- app/Http/Controllers/AccountPasswordController.php
+++ app/Http/Controllers/AccountPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 
 class AccountPasswordController extends Controller
 {
@@ -13,21 +14,27 @@ class AccountPasswordController extends Controller
 
     public function update(Request $request)
     {
-    //     $request->validate([
-    //         'current_password' => 'required',
-    //         'new_password' => 'required|min:8|confirmed',
-    //     ]);
+        $request->validate([
+            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+        ],[
+            'password.required' => '新しいパスワードを入力してください。',
+            'password.string' => '新しいパスワードは文字列で入力してください',
+            'password.confirmed' => '新しいパスワードと確認用パスワードが一致しません',
+            'password.min' => '新しいパスワードは8文字以上で入力してください',
+            'password.mixedCase' => '新しいパスワードは大文字と小文字をそれぞれ1文字以上含めてください',
+            'password.uncompromised' => '新しいパスワードは過去に流出したことのあるパスワードです。別のパスワードを設定してください',
+        ]);
 
-    //     $user = $request->user();
+        //     $user = $request->user();
 
-    //     if (!\Hash::check($request->current_password, $user->password)) {
-    //         return back()->withErrors(['current_password' => 'Current password is incorrect']);
-    //     }
+        //     if (!\Hash::check($request->current_password, $user->password)) {
+        //         return back()->withErrors(['current_password' => 'Current password is incorrect']);
+        //     }
 
-    //     $user->password = \Hash::make($request->new_password);
-    //     $user->save();
+        //     $user->password = \Hash::make($request->new_password);
+        //     $user->save();
 
-    //     return redirect()->route('dashboard')->with('status', 'Password updated successfully');
+        //     return redirect()->route('dashboard')->with('status', 'Password updated successfully');
     }
 
     public function done()
