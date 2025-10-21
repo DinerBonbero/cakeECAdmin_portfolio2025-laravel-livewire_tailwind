0e24a50246b93e0dc9c95f45fc8e2391fc939219
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-21 23:58:06 +0900

    整形、説明コメントの追記

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,16 +8,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Cart extends Model
 {
     protected $guarded = ['id'];
+    //idの保護
 
-    public $timestamps = false;//Laravelでは自動でcreated_atとupdated_atが記録される、今回のカートテーブルは取り扱わないためLaravelに自動処理をさせない処理
+    public $timestamps = false;
+    //Laravelでは自動でcreated_atとupdated_atが記録される、今回のカートテーブルは取り扱わないためLaravelに自動処理をさせない処理
 
     public function user(): BelongsTo
     {
+
         return $this->belongsTo(User::class);
     }
 
     public function item(): BelongsTo
     {
+        
         return $this->belongsTo(Item::class);
     }
 }
