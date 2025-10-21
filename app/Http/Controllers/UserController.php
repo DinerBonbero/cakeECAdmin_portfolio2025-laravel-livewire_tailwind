3603f7a90db3a712b7743c2b1a829c8dfebc66a9
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-21 22:49:08 +0900

    整形、説明コメントの追記

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\UserInfoRequest;
 use App\Models\UserInfo;
 use Illuminate\Support\Facades\Auth;
@@ -14,24 +13,23 @@ class UserController extends Controller
 
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
 
-        // dd($userInfo);
-        // exit();
         if (!isset($userInfo)) {
+            //ログインユーザーのユーザー情報がなければ
 
             return view('user_info.create');
+            //ユーザー情報登録画面を表示
         } else {
 
             return redirect()->route('items.index');
+            //ユーザー情報が既に登録されている場合、商品一覧画面へリダイレクト
         }
     }
 
     public function store(UserInfoRequest $request)
     {
 
-        $validated = $request->validated(); //連想配列
-        // var_dump($validated);
-        // $validated['first_name'];
-        // exit();
+        $validated = $request->validated();
+        //フォームリクエストバリデーション、$validatedは連想配列
 
         UserInfo::create([
             'user_id' => Auth::id(),
@@ -43,8 +41,10 @@ class UserController extends Controller
             'street_address' => $validated['street_address'],
             'address_detail' => $validated['address_detail'],
         ]);
+        //ユーザー情報登録
 
         return redirect()->route('items.index');
+        //商品一覧画面へリダイレクト
     }
 
     public function edit()
@@ -52,14 +52,15 @@ class UserController extends Controller
 
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
 
-        // dd($userInfo);
-        // exit();
         if (isset($userInfo)) {
+            //ログインユーザーのユーザー情報があれば
 
             return view('user_info.edit', compact('userInfo'));
+            //ユーザー情報編集画面を表示かつユーザー情報を渡す
         } else {
 
             return redirect()->route('items.index');
+            //ユーザー情報が登録されていない場合、商品一覧画面へリダイレクト
         }
     }
 
@@ -67,6 +68,7 @@ class UserController extends Controller
     {
 
         $validated = $request->validated();
+        //フォームリクエストバリデーション、$validatedは連想配列
 
         $userInfo = UserInfo::where('user_id', Auth::id())->first();
 
@@ -79,7 +81,9 @@ class UserController extends Controller
             'street_address' => $validated['street_address'],
             'address_detail' => $validated['address_detail'],
         ]);
+        //ユーザー情報更新
 
         return redirect()->back();
+        //ユーザー情報編集画面へリダイレクト
     }
 }
