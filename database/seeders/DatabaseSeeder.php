94c375d9e3b0cc327838dfd9ba6c0a3cfdef3970
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-28 13:13:53 +0900

    ユーザー情報シーダの呼び出しコード記述

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserInfo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class, //ユーザのマスタシーダ
             OrderSeeder::class, //注文と注文詳細のマスタシーダ、注文詳細シーダで分けて連動した処理での作成は不可のため一緒に作成
             ItemSeeder::class, //商品のマスタシーダ
+            UserInfoSeeder::class//ユーザー情報のマスタシーダ
         ]);
 
 
