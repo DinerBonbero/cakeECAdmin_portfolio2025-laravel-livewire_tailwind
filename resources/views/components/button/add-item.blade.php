38e70e5b7d003d80456ee5228e5d604a7316ea21
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-29 12:40:18 +0900

    ボタンの角丸のレスポンシブデザイン追加

--- resources/views/components/button/add-item.blade.php
+++ resources/views/components/button/add-item.blade.php
@@ -1,3 +1,3 @@
-<button class="py-1 @can('user') bg-[#7cc7f4] @else @guest bg-gray-300  @endguest @endcan text-white rounded-xl w-full border-3 border-solid border-gray-200" @guest disabled @endguest>
+<button class="py-1 @can('user') bg-[#7cc7f4] @else @guest bg-gray-300  @endguest @endcan text-white rounded-md min-[570px]:rounded-xl w-full border-3 border-solid border-gray-200" @guest disabled @endguest>
     カートに入れる
 </button>
