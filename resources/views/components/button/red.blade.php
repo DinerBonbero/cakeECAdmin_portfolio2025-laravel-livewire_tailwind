d2fd97ef47a963a37516852f45849d61a60a3310
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-10-29 12:41:22 +0900

    ボタンの角丸のレスポンシブデザイン追加

--- resources/views/components/button/red.blade.php
+++ resources/views/components/button/red.blade.php
@@ -1,3 +1,3 @@
-<button class="py-1 bg-red-500 text-white rounded-xl w-full border-3 border-solid border-gray-200">
+<button class="py-1 bg-red-500 text-white rounded-md min-[570px]:rounded-xl w-full border-3 border-solid border-gray-200">
     {{ $slot }}
 </button>
