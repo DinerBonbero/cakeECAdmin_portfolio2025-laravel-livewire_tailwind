21d1bb2e85442d78ae361aab8ded7aac085e7b4d
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-11-04 13:15:13 +0900

    ログメッセージの削除

--- app/Http/Middleware/AdminCheckMiddleware.php
+++ app/Http/Middleware/AdminCheckMiddleware.php
@@ -21,9 +21,6 @@ class AdminCheckMiddleware
             //次に進める
         } elseif (Auth::check() && Auth::user()->is_admin === 0) {
             //ログインユーザーのis_adminカラムが0、つまり一般ユーザーの場合
-            //もし不正アクセスユーザーの機密情報をログに出力する際は書き手側：サーバーアプリケーション(Apache)でノート側のシスログなどに出力する。
-            //必ず機密情報はLaravel側ではなくサーバー側で記録する！
-            //Laravel側では情報漏洩やgitignoreの設定が誤っていた時リモートに上げてしまう可能性がある
 
             return redirect()->route('errors.error');
             //エラーページへリダイレクト
