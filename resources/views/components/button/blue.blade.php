f6815bd85211841543028dc2d8b4fef0ba9afc35
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2025-11-17 22:55:34 +0900

    削除：視認性向上のため、ボタンコンポーネントに共通しているcssは記述して独自の色や幅,リンクなどの属性はコンポーネントへ渡す形式に変更

--- resources/views/components/button/blue.blade.php
+++
@@ -1,3 +0,0 @@
-<button class="bg-[#7cc7f4] py-1 w-full text-white rounded-xl border-3 border-solid border-gray-200">
-    {{ $message }}
-</button>
--- resources/views/components/button/brown.blade.php
+++
@@ -1,3 +0,0 @@
-<button class="py-1 px-10 bg-[#e2bc96] text-white rounded-xl border-3 border-solid border-gray-200">
-    {{ $message }}
-</button>
